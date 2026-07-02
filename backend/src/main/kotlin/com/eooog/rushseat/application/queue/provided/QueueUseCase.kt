ac026ab79b70c0583c1a7950848e892e8147552d
eooog <eooog@protonmail.com>
2026-07-02 13:25:23 +0900

    feat(queue): add queue provided ports

---
+++ backend/src/main/kotlin/com/eooog/rushseat/application/queue/provided/QueueUseCase.kt
@@ -0,0 +1,26 @@
+package com.eooog.rushseat.application.queue.provided
+
+import com.eooog.rushseat.application.queue.AdmitQueueCommand
+import com.eooog.rushseat.application.queue.AdmitQueueResult
+import com.eooog.rushseat.application.queue.AdmittedMember
+import com.eooog.rushseat.application.queue.EnterQueueCommand
+import com.eooog.rushseat.application.queue.GetQueueStatusQuery
+import com.eooog.rushseat.application.queue.QueueEnterResult
+import com.eooog.rushseat.application.queue.QueueStatusResult
+import com.eooog.rushseat.application.queue.ValidateAdmissionCommand
+
+interface EnterQueueUseCase {
+    fun enter(command: EnterQueueCommand): QueueEnterResult
+}
+
+interface GetQueueStatusUseCase {
+    fun getStatus(query: GetQueueStatusQuery): QueueStatusResult
+}
+
+interface AdmitQueueUseCase {
+    fun admit(command: AdmitQueueCommand): AdmitQueueResult
+}
+
+interface ValidateAdmissionUseCase {
+    fun requireAdmitted(command: ValidateAdmissionCommand): AdmittedMember
+}
